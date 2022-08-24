8ba12dbc503da2fce69208f8e4ce2544cfe91e2b
muhtomakin <muhtomakin@gmail.com>
2022-08-24 15:08:27 +0200

    operators

---
+++ operators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace variables{
+    class Program {
+        static void Main(string[] args) {
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if(isSuccess && isCompleted) {
+                Console.WriteLine("Perfect!");
+            }
+
+            if(isSuccess || isCompleted) {
+                Console.WriteLine("Great!");
+            }
+
+            if(isSuccess && !isCompleted) {
+                Console.WriteLine("Fine!");
+            }
+
+            int a = 3;
+            int b = 4;
+            bool result = a<b;
+            Console.WriteLine(result);
+            result = a>=b;
+            Console.WriteLine(result);
+        }
+    }
+}
